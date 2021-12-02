b670016f6ec0f774d40897201d982187c4351757
Colby Dehart <colbydehart@gmail.com>
2021-12-02 17:51:07 -0600

    init

---
+++ test/advent/day_one_test.exs
@@ -0,0 +1,13 @@
+defmodule Advent.DayOneTest do
+  use ExUnit.Case
+  doctest Advent.DayOne
+
+  # :code.priv_dir(:advent)
+  # |> Path.join("./day_one_input_1.txt")
+  # |> File.read!()
+  # |> String.split()
+  # |> Enum.map(&Integer.parse/1)
+  # |> Enum.map(&elem(&1, 0))
+  # |> Advent.DayOne.part_one()
+  # >> 1681
+end
